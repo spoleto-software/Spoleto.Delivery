610b05badfbfd8f6b59bc1ff67741cc3a835caff
o.nadymov <o.nadymov@spoleto.ru>
2024-09-19 10:41:14 +0300

    Rename DeliveryMode enum items.

--- src/Spoleto.Delivery/Enums/DeliveryMode.cs
+++ src/Spoleto.Delivery/Enums/DeliveryMode.cs
@@ -11,54 +11,54 @@ namespace Spoleto.Delivery
         /// Дверь-дверь
         /// </summary>
         [Description("Дверь-дверь")]
-        DoorToDoor = 1,
+        DoorDoor = 1,
 
         /// <summary>
         /// Дверь-склад
         /// </summary>
         [Description("Дверь-склад")]
-        DoorToWarehouse = 2,
+        DoorWarehouse = 2,
 
         /// <summary>
         /// Склад-дверь
         /// </summary>
         [Description("Склад-дверь")]
-        WarehouseToDoor = 3,
+        WarehouseDoor = 3,
 
         /// <summary>
         /// Склад-склад
         /// </summary>
         [Description("Склад-склад")]
-        WarehouseToWarehouse = 4,
+        WarehouseWarehouse = 4,
 
         /// <summary>
         /// Дверь-постамат
         /// </summary>
         [Description("Дверь-постамат")]
-        DoorToPostamat = 6,
+        DoorPostamat = 6,
 
         /// <summary>
         /// Склад-постамат
         /// </summary>
         [Description("Склад-постамат")]
-        WarehouseToPostamat = 7,
+        WarehousePostamat = 7,
 
         /// <summary>
         /// Постамат-дверь
         /// </summary>
         [Description("Постамат-дверь")]
-        PostamatToDoor = 8,
+        PostamatDoor = 8,
 
         /// <summary>
         /// Постамат-склад
         /// </summary>
         [Description("Постамат-склад")]
-        PostamatToWarehouse = 9,
+        PostamatWarehouse = 9,
 
         /// <summary>
         /// Постамат-постамат
         /// </summary>
         [Description("Постамат-постамат")]
-        PostamatToPostamat = 10
+        PostamatPostamat = 10
     }
 }
