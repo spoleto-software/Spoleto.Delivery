b56962aa14e0f307ab3a0017855ed091ee978ab5
o.nadymov <o.nadymov@spoleto.ru>
2024-11-12 15:21:08 +0300

    Cdek: custom NotFound method.

--- src/Providers/Spoleto.Delivery.Cdek/Providers/CdekClient.cs
+++ src/Providers/Spoleto.Delivery.Cdek/Providers/CdekClient.cs
@@ -1,11 +1,14 @@
 ﻿using Spoleto.RestClient;
 using Spoleto.RestClient.Authentication;
+using Spoleto.RestClient.Serializers;
 
 namespace Spoleto.Delivery.Providers.Cdek
 {
     public class CdekClient : RestHttpClient
     {
         private readonly CdekOptions _cdekOptions;
+
+        private readonly HashSet<string> _orderNotFoundCodes = ["v2_entity_not_found", "v2_order_not_found"];
 
         public CdekClient() : this(CdekOptions.Demo)
         {
@@ -35,6 +38,28 @@ namespace Spoleto.Delivery.Providers.Cdek
             var authenticator = new CdekAuthenticator(authCredentials);
 
             return authenticator;
+        }
+
+        public override bool NotFound(IRestResponse restResponse)
+        {
+            if (restResponse.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var content = restResponse.GetContent();
+
+                if (content is string json
+                   && !string.IsNullOrEmpty(json))
+                {
+                    var objectResult = SerializationManager.Deserialize<DeliveryOrder>(restResponse);
+                    var errorCodes = objectResult?.Requests?.Where(x => x.Errors != null).SelectMany(x => x.Errors!).Select(x => x.Code);
+                    if (errorCodes != null &&
+                        _orderNotFoundCodes.Any(x => errorCodes.Contains(x)))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return base.NotFound(restResponse);
         }
     }
 }
